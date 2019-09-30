6beb3a132d62c9327d3ccfe1262f652947523d09
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-30 15:20:35 -0500

    deleting user by id successfully

--- index.js
+++ index.js
@@ -12,28 +12,59 @@ server.use(express.json());//Need this for post and put
 
 server.get('/', (req, res) => {
     res.send('hello from node-api1-project')
+    
 })
 
-//add user with post and Insert api method
+
 server.post('/api/users', (req, res) => {
     const userData = req.body;
 
+    //add user with post and Insert api method
     users.insert(userData).then(user => {
         res.json(user)
+
     }).catch(err => {
         res.json({ message: 'error adding user' });
     });
 });
 
 server.get('/api/users', (req,res) => {
+
     //get a list of users from the database
     users.find().then(user => {
         //send the list of users to the client
         res.send(user);
     }).catch(err => {
         res.send(err)
-    })
-})
+    });
+});
+
+
+server.get('/api/users/:id', (req,res) => {
+    const id = req.params.id;
+
+    //get a user by id and return user object
+    users.findById(id).then(user => {
+        res.json(user);
+
+    }).catch(err => {
+        res.json({ message: 'error finding user by id' });
+    });
+});
+
+server.delete('/api/users/:id', (req,res) => {
+    const id = req.params.id;
+
+    //delete a user by id and return deleted user
+    users.remove(id).then(user => {
+        res.json(user);
+
+    }).catch(err => {
+        res.json({ message: 'error deleting user by id' });
+    });
+});
+
+
 
 const port = 8000;
 //listening for connections
