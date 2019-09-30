0478b7265bc92cb1bb15a0b7567cda4a83b24849
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-30 14:33:42 -0500

    successfully added user to database, one to the nine

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ const users = require('./data/db.js');
 //creates express server
 const server = express();
 
+//teaches server to speak JSON
+server.use(express.json());//Need this for post and put
+
+server.get('/', (req, res) => {
+    res.send('hello from node-api1-project')
+})
+
+//add user with post and Insert api method
+server.post('/api/users', (req, res) => {
+    const userData = req.body;
+
+    users.insert(userData).then(user => {
+        res.json(user)
+    }).catch(err => {
+        res.json({ message: 'error adding user' });
+    });
+});
+
 const port = 8000;
-
-
-
 //listening for connections
 server.listen(port, () => console.log(`\nlistening on port ${port}\n`))
