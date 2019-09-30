72fe52278731092162c1a2303b323a87081ecf33
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-09-30 13:58:40 -0500

    server is listening on port 8000

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+const express = require('express');
+
+//imports user module which contains our api methods
+const users = require('./data/db.js');
+
+//creates express server
+const server = express();
+
+const port = 8000;
+
+
+
+//listening for connections
+server.listen(port, () => console.log(`\nlistening on port ${port}\n`))
